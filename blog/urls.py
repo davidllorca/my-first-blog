1ca7ac49edba2b5a70ff1bafe09a045a4cc36e4b
David Llorca <davidllorcabaron@gmail.com>
2015-10-02 13:26:49 +0200

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views # Import all views of 'blog'
 
 urlpatterns = [
 	url(r'^$', views.post_list),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail), 
+
 ]
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')), # import blogs url on main page
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('blog.urls')),
 ]
